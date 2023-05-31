e480b1c4c3bb0c68433ffd005dc1b7d58ab99a0f
mimijae <qzqzcaraz@naver.com>
2023-06-01 05:33:56 +0900

    v1

---
+++ UIUX_Design/design_5/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:design_5/pallete.dart';
+import 'package:flutter/material.dart';
+
+import 'login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Pallete.backgroundColor,
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
